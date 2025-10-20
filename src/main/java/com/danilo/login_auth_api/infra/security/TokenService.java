a1d296cb783ceda5df39c628d2f4356b879f83bb
Danilo Rocha <danilo.rochaa@gmail.com>
2025-10-20 17:41:48 -0300

    Cria o serviço de geração e validação do token de autenticação

---
+++ src/main/java/com/danilo/login_auth_api/infra/security/TokenService.java
@@ -0,0 +1,56 @@
+package com.danilo.login_auth_api.infra.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.danilo.login_auth_api.domain.entities.User;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer("login-auth-api")
+                    .withSubject(user.getUserAuth().getEmail())
+                    .withExpiresAt(generateExpirationTime())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao gerar token", e);
+        }
+
+    }
+
+    public String validatetoken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationTime() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("UTC-3"));
+    }
+
+}
