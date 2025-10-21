3dc9c3bc7bf137c6a6e55053b25110d8caff45c4
Danilo <danilo.rochaa@gmail.com>
2025-10-21 02:19:37 -0300

    Filtro de segurança para verificar se há um token JWT válido

--- src/main/java/com/danilo/login_auth_api/domain/repositories/UserAuthRepository.java
+++ src/main/java/com/danilo/login_auth_api/domain/repositories/UserAuthRepository.java
@@ -1,7 +1,11 @@
 package com.danilo.login_auth_api.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.danilo.login_auth_api.domain.entities.UserAuth;
 
 public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
+
+    Optional<UserAuth> findByEmail(String email);
 }
---
+++ src/main/java/com/danilo/login_auth_api/infra/security/SecurityFilter.java
@@ -0,0 +1,56 @@
+package com.danilo.login_auth_api.infra.security;
+
+import java.io.IOException;
+import java.security.Security;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.danilo.login_auth_api.domain.entities.UserAuth;
+import com.danilo.login_auth_api.domain.repositories.UserAuthRepository;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserAuthRepository userAuthRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        var login = tokenService.validatetoken(token);
+
+        if (login != null) {
+            UserAuth userAuth = this.userAuthRepository.findByEmail(login)
+                    .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+            var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+            var authentication = new UsernamePasswordAuthenticationToken(userAuth, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null)
+            return null;
+        return authHeader.replace("Bearer ", "");
+
+    }
+
+}
