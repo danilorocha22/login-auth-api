df33cdb89dc099d6d443532a1bdf79ac9ef0ddf7
Danilo <danilo.rochaa@gmail.com>
2025-10-27 10:37:53 -0300

    Configuração da lib Dotenv para carregar as variáveis de amb

--- src/main/java/com/danilo/login_auth_api/LoginAuthApiApplication.java
+++ src/main/java/com/danilo/login_auth_api/LoginAuthApiApplication.java
@@ -3,10 +3,15 @@ package com.danilo.login_auth_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LoginAuthApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+				.systemProperties()
+				.load();
 		SpringApplication.run(LoginAuthApiApplication.class, args);
 	}
 
